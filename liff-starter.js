6f761a88d06d161c41ec29e174c724ddf3f79091
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-09 15:49:22 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -9,7 +9,7 @@ function initializeApp(data) {
         var msg = document.getElementById('bchcode').value;
         var xhttp = new XMLHttpRequest();
         
-        var x ;
+        var x = null ;
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 x = this.responseText;
