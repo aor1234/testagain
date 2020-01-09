c6b94d5b518398da3b16885b90ab04eefd1e4f05
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-09 14:39:50 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,36 +1,35 @@
-window.onload = function (e) {
-    liff.init(function (data) {
+window.onload = function(e) {
+    liff.init(function(data) {
         initializeApp(data);
-
     });
 };
 
 function initializeApp(data) {
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
+    document.getElementById('sendmessagebutton').addEventListener('click', function() {
         var msg = document.getElementById('bchcode').value;
-        
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("demo").innerHTML = this.responseText;
-        }
-      };
-      xhttp.open("POST", "https://damp-bayou-88423.herokuapp.com/liff", true);
-      xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      xhttp.send();
+        var http = new XMLHttpRequest();
+        var url = "https://damp-bayou-88423.herokuapp.com/liff";
+        var params;
 
-        
-        
-        
+        http.open("POST", "https://damp-bayou-88423.herokuapp.com/liff", true);
+
+
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                params = http.responseText;
+            }
+        }
+        http.send(params);
+
+
+
         liff.sendMessages([{
             type: 'text',
-            text: xhttp
-        }
-          ]).then(function () {
+            text: params
+        }]).then(function() {
             liff.closeWindow();
-        }).catch(function (error) {
+        }).catch(function(error) {
             window.alert("Error sending message: " + error);
         });
     });
 }
-
