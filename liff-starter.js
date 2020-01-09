0411c0cc4e04abba11a63891edd3663f2df0b790
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-09 14:52:45 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -8,18 +8,18 @@ function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function() {
         var msg = document.getElementById('bchcode').value;
         var http = new XMLHttpRequest();
+        var url = "https://damp-bayou-88423.herokuapp.com/liff";
         var params;
 
-        http.open("POST",http, true);
+        http.open("POST", url, true);
 
 
         http.onreadystatechange = function() {
             if (http.readyState == 4 && http.status == 200) {
-                params = http.responseText;
+                alert(http.responseText);
             }
         }
         http.send(params);
-
 
 
         liff.sendMessages([{
