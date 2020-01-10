f918fb4d4d9b40abfc8dcae904de295c338e9020
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-10 09:41:03 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,3 +1,45 @@
+// window.onload = function(e) {
+//     liff.init(function(data) {
+//         initializeApp(data);
+//     });
+// };
+
+// function initializeApp(data) {
+//     document.getElementById('sendmessagebutton').addEventListener('click', function() {
+//         var msg = document.getElementById('bchcode').value;
+//         var xhttp = new XMLHttpRequest();
+        
+//         var x ;
+//         xhttp.onreadystatechange = function() {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 var x = this.responseText;
+// //                 var jsonResponse = JSON.parse(x);
+// //                 console.log(jsonResponse["x"]);
+            
+//             }
+//         };
+//         xhttp.open("POST", "https://damp-bayou-88423.herokuapp.com/liff", true);
+        
+//         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//         xhttp.send(null);
+
+
+
+
+//         liff.sendMessages([{
+//             type: 'text',
+//             text: x
+//         }]).then(function() {
+//             liff.closeWindow();
+//         }).catch(function(error) {
+//             window.alert("Error sending message: " + error);
+//         });
+//     });
+
+    
+// }
+
+
 window.onload = function(e) {
     liff.init(function(data) {
         initializeApp(data);
@@ -8,27 +50,24 @@ function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function() {
         var msg = document.getElementById('bchcode').value;
         var xhttp = new XMLHttpRequest();
-        
-        var x ;
-        xhttp.onreadystatechange = function() {
+        var res;
+
+        xhttp.open("POST", "https://damp-bayou-88423.herokuapp.com/liff", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send();
+
+        xhttp.onreadystatechange = function(res) {
             if (this.readyState == 4 && this.status == 200) {
-                var x = this.responseText;
-//                 var jsonResponse = JSON.parse(x);
-//                 console.log(jsonResponse["x"]);
-            
+                x = this.responseText(res);
             }
         };
-        xhttp.open("POST", "https://damp-bayou-88423.herokuapp.com/liff", true);
-        
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send(null);
 
 
 
 
         liff.sendMessages([{
             type: 'text',
-            text: x
+            text: res
         }]).then(function() {
             liff.closeWindow();
         }).catch(function(error) {
@@ -36,5 +75,5 @@ function initializeApp(data) {
         });
     });
 
-    
+
 }
